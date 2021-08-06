2b77d97f46428870e8ad17e794290cf50b17412b
moeka kitamura <moeka.k76@gmail.com>
2021-08-06 17:53:07 +0900

    step3

--- vm.rb
+++ vm.rb
@@ -1,7 +1,7 @@
 # このコードをコピペしてrubyファイルに貼り付け、そのファイルをirbでrequireして実行しましょう。
 # 例
 # irb
-# require '/Users/kitamuramoeka/workspace/vending_machine/vm.rb'
+# require '/Users/kitamuramoeka/workspace/vending_machine/vm.rb'  pwdでパスを確認できる
 # （↑のパスは、自動販売機ファイルが入っているパスを指定する）
 # 初期設定（自動販売機インスタンスを作成して、vmという変数に代入する）
 # vm = VendingMachine.new
@@ -13,15 +13,22 @@
 # vm.return_money
 class VendingMachine
   # ステップ０　お金の投入と払い戻しの例コード
-  # ステップ１　扱えないお金の例コード
+  # ステップ１　扱えないお金の例コードorb
   # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
   MONEY = [10, 50, 100, 500, 1000].freeze #中身を変更したくない。予期せぬ挙動を防ぐ
   # （自動販売機に投入された金額をインスタンス変数の @slot_money に代入する）
   def initialize #initializeメソッドは自動で適用される！！！！！！引数を期待していないコンストラクタ
+    #インスタンス化するたびに初期値に戻る
     # 最初の自動販売機に入っている金額は0円
     @slot_money = 0
+    # 使いたい変数は初めにinitializeに初期値を設定しないと使えない
+    @sales_money = 0
+    @coke = {price:120, name:'coke', stock: 5}
   end
-  # 投入金額の総計を取得できる。
+  def coke
+    @coke
+  end
+  #exit投入金額の総計を取得できる。
   def current_slot_money
     # 自動販売機に入っているお金を表示する
     @slot_money #@がついてるとグローバル変数
@@ -42,4 +49,20 @@ class VendingMachine
     # 自動販売機に入っているお金を0円に戻す
     @slot_money = 0
   end
+  def buy
+    if @slot_money >= @coke[:price] && @coke[:stock] > 0
+      @coke[:stock]-=1
+      @sales_money += @coke[:price]
+      @slot_money -= @coke[:price]
+      puts "お買い上げありがとうございます！！"
+    else
+      return false
+    end
+  end
+  # 今の売上金が見たいとき、(vm.sales)
+  def sales
+    @sales_money
+  end
+
 end
+
